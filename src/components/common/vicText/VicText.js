05e67e4349a806b69e1c5ad9ae6bb95cba544961
DESKTOP-GMTST5N\Sunrise <pure90719@gmail.com>
2023-08-15 17:28:56 +0800

    add input rule

--- src/components/common/vicText/VicText.js
+++ src/components/common/vicText/VicText.js
@@ -1,11 +1,31 @@
-import { useEffect, useRef, useState } from "react";
+import { useCallback, useEffect, useRef, useState } from "react";
+import RemoveRedEyeOutlinedIcon from "@mui/icons-material/RemoveRedEyeOutlined";
+import VisibilityOffOutlinedIcon from "@mui/icons-material/VisibilityOffOutlined";
 import "./vicText.scss";
+
+const PasswordSwitch = ({ inputType, switchInputType }) => {
+    return (
+        <>
+            {inputType === "password" || (
+                <RemoveRedEyeOutlinedIcon
+                    className='vic_text__icon'
+                    onClick={switchInputType}
+                />
+            )}
+            {inputType === "text" || (
+                <VisibilityOffOutlinedIcon
+                    className='vic_text__icon'
+                    onClick={switchInputType}
+                />
+            )}
+        </>
+    );
+};
 
 const VicText = ({
     type = "text",
     showPassword = false,
     rule,
-    validation,
     disabled = false,
     label = "",
     placeholder = "",
@@ -18,6 +38,10 @@ const VicText = ({
 
     const [inputType, setInputType] = useState(type);
 
+    const [inputClass, setInputClass] = useState("vic_text");
+
+    const [testResult, setTestResult] = useState(true);
+
     const switchInputType = () => {
         if (!showPassword) return;
 
@@ -26,11 +50,27 @@ const VicText = ({
         setInputType(targetType);
     };
 
+    const handler = useCallback(
+        (e, callback) => {
+            const { value } = e.target;
+
+            if (rule instanceof RegExp) {
+                const tr = rule.test(value);
+
+                setTestResult(tr);
+            }
+
+            if (typeof rule === "function") rule(value, setTestResult);
+
+            if (typeof callback === "function") callback(e);
+        },
+        [rule]
+    );
+
     useEffect(() => {
         const input = inputRef.current;
 
         const events = {
-            change: onChange,
             input: onInput,
             focus: onFocus,
             blur: onBlur,
@@ -40,10 +80,11 @@ const VicText = ({
             Object.entries(events).forEach(([key, callback]) => {
                 if (typeof callback !== "function") return;
 
-                if (action === "add") input.addEventListener(key, callback);
+                if (action === "add")
+                    input.addEventListener(key, (e) => handler(e, callback));
 
                 if (action === "remove")
-                    input.removeEventListener(key, callback);
+                    input.removeEventListener(key, (e) => handler(e, callback));
             });
         };
 
@@ -52,18 +93,31 @@ const VicText = ({
         return () => {
             eventRegister("remove");
         };
-    }, [inputRef, onChange, onInput, onFocus, onBlur]);
+    }, [inputRef, onInput, onFocus, onBlur, handler]);
+
+    useEffect(() => {
+        testResult
+            ? setInputClass("vic_text")
+            : setInputClass(inputClass + " test_fail");
+    }, [testResult]);
 
     return (
-        <label className="vic_text">
-            <div className="vic_text__label">{label}</div>
+        <label className={inputClass}>
+            <div className='vic_text__label'>{label}</div>
             <input
                 ref={inputRef}
-                className="vic_text__input"
+                className='vic_text__input'
                 type={inputType}
                 readOnly={disabled}
                 placeholder={placeholder}
+                onChange={(e) => handler(e, onChange)}
             />
+            {!showPassword || (
+                <PasswordSwitch
+                    inputType={inputType}
+                    switchInputType={switchInputType}
+                />
+            )}
         </label>
     );
 };
--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -4,11 +4,34 @@ const Home = () => {
     const handler = (e) => {
         console.log(e);
     };
+
+    const rule = /^(?=.*[A-Z])(?!.*\s)[A-Za-z0-9]{8,16}$/;
+
+    const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+
+    const validator = (val, resolve) => {
+        if (val.includes("fuck")) resolve(false);
+        else resolve(true);
+    };
+
     return (
         <>
             <h1>home page</h1>
             <p>lorem lorem lorem lorem lorem lorem lorem lorem </p>
-            <VicText label="Enter Text" />
+            <VicText
+                label='Enter Email'
+                rule={emailRegex}
+            />
+            <VicText
+                label='Enter Password'
+                type='password'
+                showPassword={true}
+                rule={rule}
+            />
+            <VicText
+                label='Enter No F Words'
+                rule={validator}
+            />
         </>
     );
 };
